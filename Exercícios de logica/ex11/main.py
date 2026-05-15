bb3ac11a77aa3f42421accfc0c9ca5733347924b
Alex.byte <alexguimaraes.dev@gmail.com>
2026-05-14 21:54:53 -0300

    ex11

--- Exercícios de logica/ex11/main.py
+++ Exercícios de logica/ex11/main.py
@@ -1,2 +1,20 @@
 """ Faça um algoritmo que leia quatro notas obtidas por um aluno, calcule a média das nota obtidas, imprima na tela o nome do aluno e 
  se o aluno foi aprovado ou reprovado. Para o aluno ser considerado aprovado sua média final deve ser maior ou igual a 7. """
+
+name = input('Seu Nome:')
+
+print("======================================NOTA======================================")
+
+luffy = float(input('1º Bimestre'))
+zoro = float(input('2º Bimestre'))
+sanji = float(input('3º Bimestre'))
+jimbe = float(input('4º Bimestre'))
+
+mul = luffy + zoro + sanji + jimbe / 3
+
+print (round(mul,1))
+
+if (mul > 7):
+    print('Parabens {}! Você foi APROVADO!!'.format(name))
+else:
+    print('Infelizmente {}, Você foi REPROVADO!!'.format(name))
