d519a885c7df293fb8f3872989ccca81d2e80992
Alex.byte <alexguimaraes.dev@gmail.com>
2026-05-26 00:12:58 -0300

    atualização

--- Programas/laucher de jogos/main.py
+++ Programas/laucher de jogos/main.py
@@ -1,13 +1,22 @@
-import os
+import os 
+import emoji
+import time
+
+os.system('cls')
+
+print('=' * 20)
+print('-Meu Menu-')
 
 while True:
-    print('=' * 20)
-    print('1 - Discord')
-    print('2 - Steam')
-    print('3 - LoL')
-    print('4 - Navegador')
 
-    escolha = int(input('Digite:'))
+    print('=' * 20)
+    print('1 - Discord 💬')
+    print('2 - Steam 🎮')
+    print('3 - LoL ⚔️')
+    print('4 - Navegador 🌐')
+    print('0 - Sair ❌')
+
+    escolha = int(input('Escolha:'))
 
     if escolha == 1:
         os.startfile(r'C:\Users\alex0\AppData\Local\Discord\app-1.0.9238\Discord.exe')
@@ -21,5 +30,10 @@ while True:
     elif escolha == 4:
         os.startfile(r'C:\Program Files\Zen Browser\zen.exe')
 
+    elif escolha == 0:
+        print('Fechando o Laucher...')
+        time.sleep(0.8)
+        break
+            
     else:
         print('Resposta invalida!')
