7864bdd966af3f3266eaa7edb0e9c7cca59b9673
Alex.byte <alexguimaraes.dev@gmail.com>
2026-06-01 20:31:16 -0300

    cores no terminal

---
+++ Exercícios de aula/ex12/main.py
@@ -0,0 +1,31 @@
+"""
+Cor do terminal
+
+ 0  None
+1  Bold
+4  Underline
+7  Negative
+
+TEXT
+
+30 ⚪ Branco/Cinza
+31 🔴 Vermelho
+32 🟢 Verde
+33 🟡 Amarelo
+34 🔵 Azul
+35 🟣 Roxo
+36 🔷 Ciano
+37 ⚪ Cinza claro
+
+BACK
+
+40 ⬛ Fundo Preto
+41 🟥 Fundo Vermelho
+42 🟩 Fundo Verde
+43 🟨 Fundo Amarelo
+44 🟦 Fundo Azul
+45 🟪 Fundo Roxo
+46 🟦 Fundo Ciano
+47 ⬜ Fundo Cinza/Branco """
+
+print('\033[4;36mcontato.com')
