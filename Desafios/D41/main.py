9e6144013265783a4d363347c425785f4c8a3e03
Alex.byte <alexguimaraes.dev@gmail.com>
2026-06-08 17:27:32 -0300

    FOGOS!!

---
+++ Desafios/D41/main.py
@@ -0,0 +1,11 @@
+#Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+import time
+
+
+exploda = input('aperte "ENTER" para começar os fogos: ')
+
+for c in range(10, 0, -1):
+    time.sleep(0.9)
+    print(c)
+    time.sleep(0.2)
+print('FOGOS!!!!')
