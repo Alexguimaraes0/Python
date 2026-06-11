5f4d22f51e16be8fbedaff557c735881f44b7e6e
Alex.byte <alexguimaraes.dev@gmail.com>
2026-06-10 21:28:42 -0300

    D42 D43

---
+++ Desafios/D43/main.py
@@ -0,0 +1,12 @@
+#FAÇA UM PROGRAMA QUE CALCULE A SOMA ENTRE TODOS OS NÚMEROS IMPÁRES QUE SÃO MÚLTIPLOS DE TRÊS E QUE SE ENCONTRAM NO INTERVALO DE 1 ATÉ 500
+
+sum = 0
+
+for c in range(1,500,2):
+    
+    if c % 3 == 0:
+        sum += c
+    print(c)
+
+print('=' * 20)
+print(f'A soma de todos eles são: {sum}')
--- Exercícios de logica/ex32/main.py
+++ Exercícios de logica/ex32/main.py
@@ -1,6 +1,6 @@
 #Faça um programa que peça um número inteiro ao usuário e mostre a tabuada desse número de 1 a 10 usando "for".
 
-num = int(input('Digite um número de 1 a 10: '))
+num = int(input('Digite um número: '))
 
 for c in range(1, 11):
     print('{} X {} = {}'.format(num, c, num * c))
