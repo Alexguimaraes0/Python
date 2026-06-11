505636b68401522804580357c6051d3760b2d8d9
Alex.byte <alexguimaraes.dev@gmail.com>
2026-06-10 22:06:13 -0300

    atualização

--- Desafios/D47/main.py
+++ Desafios/D47/main.py
@@ -1,5 +1,1 @@
 #FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E DIGA SE ELE É OU NÃO UM NÚMERO PRIMO
-num = int(input('Digite um número: '))
-
-for num in range(num):
-    
--- Exercícios de logica/ex33/main.py
+++
@@ -1,2 +0,0 @@
-#Faça um programa que mostre na tela apenas os números pares entre 1 e 50 usando o comando for.
-
