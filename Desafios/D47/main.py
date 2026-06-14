cff3e3618485e62a6e55fec5acb936ba205f88eb
Alex.byte <alexguimaraes.dev@gmail.com>
2026-06-13 23:55:02 -0300

    ...

--- Desafios/D47/main.py
+++ Desafios/D47/main.py
@@ -1,1 +1,14 @@
 #FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E DIGA SE ELE É OU NÃO UM NÚMERO PRIMO
+
+num = int(input('Digite um número: '))
+
+primo = True
+
+for c in range(2, num):
+    if num % c == 0:
+        primo = False
+
+if primo:
+    print('É primo')
+else:
+    print('Não é primo')
