ff79564b6e592f08d251392685146ff79c690704
Alex.byte <alexguimaraes.dev@gmail.com>
2026-07-06 21:41:52 -0300

    Fatorial

--- Desafios/D55/main.py
+++ Desafios/D55/main.py
@@ -2,3 +2,15 @@
 Ex:
 5! = 5x4x3x2x1 = 120 """
 
+num = int(input("Digite um número: "))
+fatorial = 1
+i = 1
+numeros = []
+
+while i <= num:
+    fatorial = fatorial * i
+    numeros.append(i)
+    i += 1
+
+numeros.reverse()
+print(f'{num}! = {"x".join(str(n) for n in numeros)} = {fatorial}')
