e59f56395c81c394d39de862af8e965eb3c90c21
Alex.byte <alexguimaraes.dev@gmail.com>
2026-07-15 21:11:34 -0300

    Cadastro de alunos 1.0v

--- Projetos/Cadastro de alunos/main.py
+++ Projetos/Cadastro de alunos/main.py
@@ -1,5 +1,10 @@
+import time
 
 escolha = 1
+alunos = []
+soma = 0
+maior_nota = 0
+maior_nota_nome = ""
 
 while escolha != 0:
     print('=' * 10, 'MENU', '=' * 10)
@@ -12,15 +17,41 @@ while escolha != 0:
 
     escolha = int(input("Qual opção deseja? "))
 
-    alunos = []
-
-
     if escolha == 1:
         nome = input("Nome: ")
         nota = int(input("Nota: "))
         lista = [nome, nota]
-        alunos.append(lista)
-    print(alunos)
+        alunos.append(lista)    
+        soma += nota
+        if nota > maior_nota:
+            maior_nota = nota
+            maior_nota_nome = nome
 
-    if escolha == 2:
-        
+    elif escolha == 2:
+        if alunos == []:
+            print("Nenhum aluno Cadastrado!")
+        else:
+            for numero, lista in enumerate(alunos, start=1):
+                print(f'{numero} -> {lista[0]} -> {lista[1]}')
+
+
+    elif escolha == 3:
+        if alunos == []:
+            print("Nenhum aluno Cadastrado!")
+        else:
+            media = soma / len(alunos)
+            print(f'Média: {media:.2f}')
+
+
+    elif escolha == 4:
+        if alunos == []:
+            print("Nenhum aluno Cadastrado!")
+        else:
+            print('Aluno com a maior nota: {} -> {}pts.'.format(maior_nota_nome, maior_nota))
+    time.sleep(1)
+
+    if escolha == 0:
+        print('Fechando...')
+        time.sleep(1)
+        break
+
