70577947b4f0232a3cacccf62f52bcb524890f43
Alex.byte <alexguimaraes.dev@gmail.com>
2026-07-17 20:34:12 -0300

    Alguns desafios

--- Desafios/D62/main.py
+++ Desafios/D62/main.py
@@ -1,14 +1,17 @@
-""" Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999,
- que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag e usando o BREAK) . """
+""" Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
+ O programa será interrompido quando o número solicitado for negativo. """
 
-n = 0
-s = 0
-cont = 0
+num = int(input("Digite um número: "))
+c = 0
 
 while True:
-    n = int(input("Digite um número (999 para parar): "))
-    if n == 999:
+    if num < 0:
+        print("Números negativos são invalidos")
         break
-    s += n
-    cont += 1
-print(f"Foram Digitados {cont} números e a soma equivale a {s}")
+    print(f'{num} X {c}')
+    if c > 9 :
+        num = int(input("Digite outro número: "))
+
+        c = 0
+        
+    c += 1
--- Desafios/D63/main.py
+++ Desafios/D63/main.py
@@ -1,17 +1,47 @@
-""" Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário.
- O programa será interrompido quando o número solicitado for negativo. """
+""" Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o jogador PERDER,
+ mostrando o total de vitórias consecutivas que ele conquistou no final do jogo. """
+from random import randint
 
-num = int(input("Digite um número: "))
-c = 0
+print('=' * 20)
+print("Par ou Impar?")
+print('=' * 20)
+
+partidas = 0
+partidas_ganhas = 0
+vitorias = 0
+maior_vitorias = 0
 
 while True:
-    if num < 0:
-        print("Números negativos são invalidos")
-        break
-    print(f'{num} X {c}')
-    if c > 9 :
-        num = int(input("Digite outro número: "))
+    num = int(input("Digite um número: "))
+    par_impar = input(("Par ou Impar [P/I]: ")).strip().upper()
+    print('-' * 20)
 
-        c = 0
-        
-    c += 1
+    aleatorio = randint(1, 10)
+    soma = num + aleatorio
+
+    if (soma % 2) == 0:
+        resultado = "P"
+        resultado_texto = "Par"
+    else:
+        resultado = "I"
+        resultado_texto = "Impar"
+
+    print(f'Você jogou {num} e o computador jogou {aleatorio}. Total de {soma} Deu {resultado_texto}')
+    print('-' * 20)
+    partidas += 1
+
+    if par_impar == resultado:
+        print("Você Venceu!")
+        vitorias += 1
+        partidas_ganhas += 1
+    else:
+        print("Você perdeu")
+        print(f"Vitorias consecutivas: {vitorias}")
+        historico = int(input("Digite 1 para ver seu historico: "))
+        if historico == 1:
+            print("-" * 20)
+            print(f"Partidas jogadas: {partidas} \nPartidas vencidas: {partidas_ganhas}")
+        if vitorias > maior_vitorias:
+            maior_vitorias = vitorias
+            print("Parabens você atingiu seu record!")
+        break
