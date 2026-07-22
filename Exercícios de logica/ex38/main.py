4f389f75d229ef2c25175420166afab406d7959f
Alex.byte <alexguimaraes.dev@gmail.com>
2026-07-21 23:12:25 -0300

    .

---
+++ Exercícios de logica/ex38/main.py
@@ -0,0 +1,5 @@
+""" Crie um programa que simule um caixa eletrônico simplificado. O programa deve perguntar ao usuário o valor que ele deseja sacar. Sabendo que o caixa só possui notas de
+ R$50, R$20, R$10 e R$5, o programa deve informar quantas notas de cada tipo serão 
+ entregues, usando a menor quantidade de notas possível. O programa deve repetir o processo
+   até que o usuário digite 0 como valor de saque (condição de parada). """
+
