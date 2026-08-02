b0c9154efbd0298375ba10d0b8b4d98334e842ef
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-02 20:48:44 -0300

    melhorando

--- Projetos/Contador de Horas/main.py
+++ Projetos/Contador de Horas/main.py
@@ -1,19 +1,16 @@
 import time
 
 atividades = []
+tempo_gasto = 0
+escolha = ""
+
 
 while True:
-    start = input("Aperte ENTER para começar...")
+    print("Contador de Horas")
 
-    inicio = time.time()
-    end = input("Pressione Enter quando terminar...")
-    fim = time.time()
-    tempo = fim - inicio
-    print(tempo)
     print('=~' * 30)
 
     print("1 - Registrar atividade\n2 - Ver resumo do dia\n0- Sair")
-    escolha = int(input("oque deseja? "))
 
     if escolha == 1:
         atividade = input("Nome da Atividade: ")
@@ -22,9 +19,24 @@ while True:
     elif escolha == 2:
         if atividades == []:
             print("Nenhuma atividade registrada")
-        print(atividades)
+        if atividades != []:
+            print(f"{atividades}-> 1")
 
     elif escolha == 0:
         print("Encerrando...")
         time.sleep(1)
         break
+
+    print('=~' * 30)
+    if atividades != []:
+        print("Para inicar contagem pressione 4 ")
+        if escolha == 4:
+            start = input("Aperte ENTER para começar...")
+
+            inicio = time.time()
+            end = input("Pressione Enter quando terminar...")
+            fim = time.time()
+            tempo = fim - inicio
+            print(tempo)
+
+    escolha = int(input("oque deseja? "))
