33128218080041e5e19d5e1f0fb4f092048ae189
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-04 01:12:17 -0300

    avanços

--- Projetos/Contador de Horas/main.py
+++ Projetos/Contador de Horas/main.py
@@ -6,45 +6,53 @@ escolha = ""
 
 
 while True:
-    print("Contador de Horas")
 
-    print('=~' * 30)
+    if atividades == []:
 
-    print("1 - Registrar atividade\n2 - Ver resumo do dia\n3- Escolher atividade\n0- Sair")
+        atividade = input("Registre sua primeira atividade: ") 
+        atividades.append(atividade)  
+        time.sleep(0.3)
 
-    if escolha == 1:
-        atividade = input("Nome da Atividade: ")
-        nome_numero = (atividade, 1)
-        atividades.append(nome_numero)
 
-    elif escolha == 2:
-        if atividades == []:
-            print("Nenhuma atividade registrada")
+        print("1 - Registrar atividades\n2 - Ver resumo do dia\n3 - Escolher atividade\n0 - Sair")
+
+
+    else:
+
+        escolha = int(input("oque deseja? "))
+
+        if escolha == 1: 
+            atividade = input("Nome da Atividade: ")
+            atividades.append(atividade)
+
+        elif escolha == 2:
+            if atividades == []:
+                print("Nenhuma atividade registrada")
+            if atividades != []:
+                for atividade in atividades:
+                    print(f"{atividade} -> 1")
+
+        elif escolha == 3:
+            if atividades == []:
+                print("Nenhuma atividade registrada")
+            print("Atividades registradas:")
+            for indice, atividade in enumerate(atividades):
+                print(f"{(indice + 1)} -> {atividade}")
+            
+        elif escolha == 0:
+            print("Encerrando...")
+            time.sleep(1)
+            break
+
+        print('=~' * 30)
+        
         if atividades != []:
-            print(f"{atividades}-> 1")
+            print("Para inicar contagem pressione 4 ")
+            if escolha == 4:
+                start = input("Aperte ENTER para começar...")
 
-    elif escolha == 3:
-        print("Atividades registradas:")
-        for indice, atividade in enumerate(atividades):
-           print(f"{(indice + 1)} -> {atividade[0]}")
-           
-
-    elif escolha == 0:
-        print("Encerrando...")
-        time.sleep(1)
-        break
-
-    print('=~' * 30)
-    
-    if atividades != []:
-        print("Para inicar contagem pressione 4 ")
-        if escolha == 4:
-            start = input("Aperte ENTER para começar...")
-
-            inicio = time.time()
-            end = input("Pressione Enter quando terminar...")
-            fim = time.time()
-            tempo = fim - inicio
-            print(f"{tempo:.2f}")
-
-    escolha = int(input("oque deseja? "))
+                inicio = time.time()
+                end = input("Pressione Enter quando terminar...")
+                fim = time.time()
+                tempo = fim - inicio
+                print(f"{tempo:.2f}")
