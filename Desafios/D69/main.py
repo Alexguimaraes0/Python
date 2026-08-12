152c23dc9b63a27c67bbfb42a5addf04b86cc87a
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-12 20:35:37 -0300

    .

---
+++ Desafios/D69/main.py
@@ -0,0 +1,8 @@
+""" Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla. """
+import random
+
+num = random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10),random.randint(1, 10)
+print(f"Numeros sorteados:", *num, sep=" ")
+print(f"O maior número sorteado: {max(num)}")
+print(f"O menor número sorteado: {min(num)}")
