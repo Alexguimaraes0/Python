38d8704ca8f087fd6c67bec37d1381894a92cd81
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-15 20:03:27 -0300

    areas do tkinter

--- Projetos/Tkinter/sec.py
+++ Projetos/Tkinter/sec.py
@@ -6,4 +6,10 @@ janela.title("Contador de horas")
 titulo = tk.Label(janela, text="Contador de tempo")
 titulo.pack()
 
+sidebar = tk.Frame(janela, width=150, bg="red")
+sidebar.pack(side=tk.LEFT, fill=tk.Y)
+
+acentral = tk.Frame(janela, bg="Blue")
+acentral.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
+
 janela.mainloop()
