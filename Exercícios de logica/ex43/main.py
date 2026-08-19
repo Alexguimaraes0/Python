65616318af1fb12e09bd99aa99c5160690c404b4
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-18 22:18:30 -0300

    logistica de voo

---
+++ Exercícios de logica/ex43/main.py
@@ -0,0 +1,28 @@
+
+
+qtd_classe1 = 10
+qtd_classe2 = 15
+qtd_classe3 = 18
+
+passageiros_classe1 = []
+passageiros_classe2 = []
+passageiros_classe3 = []
+assentos_especias = []
+
+for i in range(qtd_classe1):
+    nome = input(f"Nome do passageiro {i+1} (primeira classe): ")
+    necessidade = input("Necessidade especial (ou nenhuma: )").strip().upper()
+    passageiros_classe1.append((nome, necessidade))
+
+for i in range(qtd_classe2):
+    nome = input(f"Nome do passageiro {i+1} (classe executiva): ")
+    necessidade = input("Necessidade especial (ou nenhuma): ").strip().upper()
+    passageiros_classe2.append((nome, necessidade))
+
+for i in range(qtd_classe3):
+    nome = input(f"Nome do passageiro: {i+1} (classe econômica): ")
+    necessidade = input("Necessidade especial (ou nenhuma): ").strip().upper()
+    passageiros_classe3.append((nome, necessidade))
+
+necessitados = [p for p in assentos_especias if p[1] != "NENHUMA"]
+assentos_especias = passageiros_classe1 + passageiros_classe2 + passageiros_classe3
