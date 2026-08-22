2c4d3ad83d6ce82805c6d81f23c81a3dfa5f0164
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-22 19:36:55 -0300

    tkinter

--- Projetos/Tkinter/sec.py
+++ Projetos/Tkinter/sec.py
@@ -2,12 +2,13 @@ import tkinter as tk
 
 janela = tk.Tk()
 janela.title("Contador de horas")
+janela.geometry('900x800')
 
 titulo = tk.Label(janela, text="Contador de tempo")
 titulo.pack()
 
-sidebar = tk.Frame(janela, width=150, bg="red")
-sidebar.pack(side=tk.LEFT, fill=tk.Y)
+sidebar = tk.Frame(janela, width=250, bg="red")
+sidebar.pack(side=tk.LEFT, fill=tk.BOTH, expand=True )
 
 acentral = tk.Frame(janela, bg="Blue")
 acentral.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
