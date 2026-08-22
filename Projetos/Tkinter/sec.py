e031ab630678e9dc8bec056b8ad108577a712c97
Alex.byte <alexguimaraes.dev@gmail.com>
2026-08-22 20:18:18 -0300

    melhorando tkinter

--- Projetos/Tkinter/sec.py
+++ Projetos/Tkinter/sec.py
@@ -2,15 +2,21 @@ import tkinter as tk
 
 janela = tk.Tk()
 janela.title("Contador de horas")
-janela.geometry('900x800')
+janela.geometry('1100x800')
+janela.minsize(700,500)
+
+janela.columnconfigure(0, weight=1)
+janela.columnconfigure(1, weight=3)
+
+janela.rowconfigure(2, weight=2)
 
 titulo = tk.Label(janela, text="Contador de tempo")
-titulo.pack()
+titulo.grid(row=1, column=0, columnspan=2)
 
-sidebar = tk.Frame(janela, width=250, bg="red")
-sidebar.pack(side=tk.LEFT, fill=tk.BOTH, expand=True )
+sidebar = tk.Frame(janela, bg="red")
+sidebar.grid(row=2, column=0, sticky='nsew')
 
 acentral = tk.Frame(janela, bg="Blue")
-acentral.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
+acentral.grid(row=2, column=1, sticky='nsew')
 
 janela.mainloop()
