0f331647a82b48e5e4d3dfc66dbe96c7912eac19
Alex.byte <alexguimaraes.dev@gmail.com>
2026-09-04 19:24:15 -0300

    prototipo_1v do cronometro

--- Projetos/Número/teste.py
+++ Projetos/Número/teste.py
@@ -4,7 +4,7 @@ import tkinter as tk
 class Cronometro:
     def __init__(self, janela):
         self.janela = janela
-        self.janela.tittle("Contador de Horas")
+        self.janela.title("Contador de Horas")
         self.janela.geometry('1100x800')
 
         janela.columnconfigure(0, weight=1)
@@ -17,4 +17,8 @@ class Cronometro:
 
         acentral = tk.Frame(janela, bg='blue')
         acentral.grid(row=1, column=1, sticky='nsew')
-        
+
+if __name__ == "__main__":
+    janela = Tk()
+    app = Cronometro(janela)
+    janela.mainloop()
