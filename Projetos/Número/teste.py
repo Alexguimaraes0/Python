3fc3a8f43dde1311a9047221b9d66a6718962451
Alex.byte <alexguimaraes.dev@gmail.com>
2026-09-04 19:50:34 -0300

    prototipo_2v

--- Projetos/Número/teste.py
+++ Projetos/Número/teste.py
@@ -7,16 +7,25 @@ class Cronometro:
         self.janela.title("Contador de Horas")
         self.janela.geometry('1100x800')
 
-        janela.columnconfigure(0, weight=1)
+        janela.columnconfigure(0, weight=2)
         janela.columnconfigure(1, weight=3)
 
         janela.rowconfigure(1, weight=2)
 
-        sidebar = tk.Frame(janela, bg="red")
+        sidebar = tk.Frame(janela, bg="#666660")
         sidebar.grid(row=1, column=0, sticky='nsew')
 
-        acentral = tk.Frame(janela, bg='blue')
-        acentral.grid(row=1, column=1, sticky='nsew')
+        self.acentral = tk.Frame(janela, bg="#020202")
+        self.acentral.grid(row=1, column=1, sticky='nsew')
+
+        self.interface()
+
+    def interface(self):
+        self.acentral.columnconfigure(0, weight=1)
+        self.acentral.rowconfigure(0, weight=1)
+        container = Frame(self.acentral, bg="#464646", width=700, height=500, highlightthickness=2.5, highlightbackground="#ffffff")
+        container.grid(row=0, column=0)
+        
 
 if __name__ == "__main__":
     janela = Tk()
