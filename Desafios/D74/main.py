a8052891516fcbc17263ea7e46f76f048e407c54
Alex.byte <alexguimaraes.dev@gmail.com>
2026-09-17 19:09:03 -0300

    .

--- Desafios/D74/main.py
+++ Desafios/D74/main.py
@@ -1,2 +1,12 @@
 """ Crie um programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista na lista, ele não será adicionado.
 No final, serão exibidos todos os valores digitados, em ordem crescente. """
+
+numeros = 0
+
+while True:
+
+    num = int(input("Digite um número: "))
+    numeros += num
+    pergunta = input("Quer continuar? [S/N] ").split().upper()
+    if pergunta == "N":
+        print(sorted(numeros))
